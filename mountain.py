10cbd5c6bcabf6433255710f6f5d5999f6eb2a14
Prateek Srivastava <pratsriv@iu.edu>
2016-11-04 09:05:24 -0400

    Gibb's sampling WIP

--- mountain.py
+++ mountain.py
@@ -9,16 +9,15 @@ from numpy import *
 from scipy.ndimage import filters
 from scipy.misc import imsave
 import sys
+import random
 
 
 # calculate "Edge strength map" of an image
 #
 def edge_strength(input_image):
     grayscale = array(input_image.convert('L'))
-    imsave('b.jpg', grayscale)
     filtered_y = zeros(grayscale.shape)
     filters.sobel(grayscale, 0, filtered_y)
-    imsave('c.jpg', filtered_y ** 2)
     return filtered_y ** 2
 
 
@@ -41,32 +40,81 @@ def draw_edge(image, y_coordinates, color, thickness):
 #
 (input_filename, output_filename, gt_row, gt_col) = sys.argv[1:]
 
+input_filename = "test_images/mountain.jpg"
+
 # load in image 
 input_image = Image.open(input_filename)
 
 # compute edge strength mask
 edge_strength = edge_strength(input_image)
-# print edge_strength
 imsave('edges.jpg', edge_strength)
 
 # You'll need to add code here to figure out the results! For now,
-# just create a horizontal centered line.
-
+col_len = 0
 row_len = len(edge_strength)
 for row in edge_strength:
     col_len = len(row)
     break
 
-# print row_len, col_len
+print col_len, row_len
+############################################
+# Question 1 of the code
 edge_list = []
 for col in range(0, col_len):
-    a = [edge_strength[row][col] for row in range(0, row_len)]
-    for x, y in enumerate(a):
-        if y == max(a):
-            edge_list.append(x)
+    col_list = [edge_strength[row][col] for row in range(0, row_len)]
+    for state_index, intensity in enumerate(col_list):
+        if intensity == max(col_list):
+            edge_list.append(state_index)
             break
 
 ridge = edge_list
+
+
+# Question 1 ends here and ridge is the answer
+##############################################
+
+# Calculate P(S_i|S_i-1) or P(S_i|S_i+1)
+def trans_prob(curr, n_curr, cur_col_list):
+    return (curr + n_curr) / (2.0 * len(cur_col_list))
+
+
+# Calculate P(W|S_i)
+def emis_prob(index, col):
+    return (col[index] + (len(col) - index)) / (255.0 * 255 * len(col))
+
+
+# Calculate P(S_i|S_i+1,S_i-1,W_i)
+def posterior_prob(trans1, trans2, emis):
+    return trans1 * trans2 * emis
+
+
+# Gibb's sampling
+for col in range(0, col_len):
+    prob_array = []
+    col_list = [int(edge_strength[row][col]) for row in range(0, row_len)]
+    for i, row in enumerate(col_list):
+        # for 1st column there is no previous state
+        if col == 0:
+            trans_prob_post = trans_prob(i, ridge[col + 1], col_list)
+            print "transition prob is ",trans_prob_post
+            emis_prob = emis_prob(i, col_list)
+            print "emis prob is ",emis_prob
+            prob_array.append(posterior_prob(1, trans_prob_post, emis_prob))
+
+        # for last column there is no next state
+        elif col == col_len - 1:
+            trans_prob_prev = trans_prob(i, ridge[col - 1], col_list)
+            emis_prob = emis_prob(i, col_list)
+            prob_array.append(posterior_prob(1, trans_prob_prev, emis_prob))
+
+        else:
+            trans_prob_prev = trans_prob(i, ridge[col - 1], col_list)
+            trans_prob_post = trans_prob(row, ridge[col + 1], col_list)
+            emis_prob = emis_prob(i, col_list)
+            prob_array.append(posterior_prob(trans_prob_prev, trans_prob_post, emis_prob))
+
+        print prob_array
+
 # ridge = [edge_strength.shape[0] / 2] * edge_strength.shape[1]
 
 # output answer
