1d7b76c30cf7d1f4bfe05aa68125ebadab638b88
ragha <ragha@ragha.com>
2016-11-06 19:33:30 -0500

    Final

--- mountain.py
+++ mountain.py
@@ -10,9 +10,12 @@ Output Color :
 		MCMC					-	blue
 		MCMC with user input	-	green
 
+Emission Probabilty : It is the edge strength normalised over the column.(Assigning higher rows with more probability is misleading, so i have used uniform proir probability)
+Transision probabitlty : It is the transition between row. Equal rows get higher probability and rows far away get less probability. I have normalised over all values of column. 
+
 In simplified we have taken only the max of the edge strength, because P(S) is constant. (i.e Apriori, each row is 
 equally likely to contain the mountain ridge)
-In MCMC we define the probability based on edge strenght and the transition from previous row and previous column. 
+In MCMC we define the probability based on edge strenght and the transition from next column and previous column. 
 
 If more importance(Weight) is given to edge strength then image which the moutains are clear are geeting good results but images with mountains out of scope the results are bad. If i give more inportance(Weight) to transistion probability then if a wrong point is calculated in picture then that is carried over to next points. So the results goes bad in clear mountains. 
 
